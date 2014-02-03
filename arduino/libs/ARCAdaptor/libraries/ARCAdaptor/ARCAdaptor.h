84e01d973be3b1c76c0f47dcbff7eb35ce6099e0
znoxx <nzabrod@yandex.ru>
2014-02-03 23:19:01 +0400

    Added initial ARCAdaptor arduino library
    ARCAdaptor pins for Arduino mapping
    just include “ARCAdaptor.h” to your sketch and start using ARCAdaptor
    pins in natural way

---
+++ arduino/libs/ARCAdaptor/libraries/ARCAdaptor/ARCAdaptor.h
@@ -0,0 +1,25 @@
+#define X1_1 13
+#define X1_3 12
+#define X1_5 A0
+#define X1_7 A1
+#define X1_9 A2
+#define X1_11 A3
+#define X1_13 A4
+#define X1_15 A5
+
+#define X2_1 7
+#define X2_3 4
+#define X2_5 8
+#define X2_7 6
+#define X2_9 5
+#define X2_11 9
+#define X2_13 10
+#define X2_15 11
+
+#define X3_1 3
+#define X3_3 1
+#ifdef RESET
+#define X3_5 RESET
+#endif
+
+
